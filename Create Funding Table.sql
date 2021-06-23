621b744c7fcc54c36a3f40272f54339c50234614
OH-kb <kalli.buchanan@ontariohealth.ca>
2021-06-23 10:28:40 -0400

    Create Funding Table Query
    Added query to create the funding table.

---
+++ Create Funding Table.sql
@@ -0,0 +1,20 @@
+-- Create a new table called '[Funding Reconciliation]' in schema '[dbo]'
+-- Drop the table if it already exists
+IF OBJECT_ID('[dbo].[Funding Reconciliation]', 'U') IS NOT NULL
+DROP TABLE [dbo].[Funding Reconciliation]
+GO
+-- Create the table in the specified schema
+CREATE TABLE [dbo].[Funding Reconciliation]
+(
+    [Id] INT NOT NULL PRIMARY KEY, -- Primary Key column
+    [Laboratory] NVARCHAR(150) NOT NULL,
+    [Month] DATE NOT NULL,
+    [Service] NVARCHAR(150) NOT NULL,
+    [ActualVolumes] INT NOT NULL,
+    [ResubmittedVolumes] INT NOT NULL,
+    [Change] INT NOT NULL,
+    [Justification] NVARCHAR(500),
+    [Created] DATE NOT NULL
+    -- Specify more columns here
+);
+GO
